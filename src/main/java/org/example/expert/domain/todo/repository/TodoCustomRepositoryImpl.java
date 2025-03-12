36f930c0a9805d3c66340f0fbe0c6b3c94631426
yoorkim <amy0848@naver.com>
2025-03-12 16:51:48 +0900

    Lv.2-8 : QueryDSL 적용

---
+++ src/main/java/org/example/expert/domain/todo/repository/TodoCustomRepositoryImpl.java
@@ -0,0 +1,31 @@
+package org.example.expert.domain.todo.repository;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import lombok.RequiredArgsConstructor;
+import org.example.expert.domain.todo.entity.QTodo;
+import org.example.expert.domain.todo.entity.Todo;
+import org.example.expert.domain.user.entity.QUser;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+@RequiredArgsConstructor
+public class TodoCustomRepositoryImpl implements TodoCustomRepository{
+
+    private final JPAQueryFactory queryFactory;
+
+    @Override
+    public Optional<Todo> findByIdWithUser(Long todoId) {
+        QTodo todo = QTodo.todo;
+        QUser user = QUser.user;
+
+        Todo result = queryFactory
+                .selectFrom(todo)
+                .leftJoin(todo.user, user).fetchJoin()
+                .where(todo.id.eq(todoId))
+                .fetchOne();
+
+        return Optional.ofNullable(result);
+    }
+}
--- src/main/java/org/example/expert/domain/todo/repository/TodoRepository.java
+++ src/main/java/org/example/expert/domain/todo/repository/TodoRepository.java
@@ -9,9 +9,8 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.time.LocalDateTime;
-import java.util.Optional;
 
-public interface TodoRepository extends JpaRepository<Todo, Long> {
+public interface TodoRepository extends JpaRepository<Todo, Long>, TodoCustomRepository {
 
     @EntityGraph(attributePaths = {"user"})
     @Query("SELECT t FROM Todo t WHERE " +
@@ -23,8 +22,4 @@ public interface TodoRepository extends JpaRepository<Todo, Long> {
                                 @Param("end") LocalDateTime end,
                                 Pageable pageable);
 
-    @Query("SELECT t FROM Todo t " +
-            "LEFT JOIN t.user " +
-            "WHERE t.id = :todoId")
-    Optional<Todo> findByIdWithUser(@Param("todoId") Long todoId);
 }
